9e9669687e9906c41480cc8c6d0de2c514eb7ebb
Martin Kröner <kronerm9@gmail.com>
2019-12-07 16:15:42 +0100

    Export types of actors (static, dynamic, weapon)

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author": "kreny",
     "description": "",
     "blender": (2, 80, 0),
-    "version": (0, 1, 0),
+    "version": (0, 1, 1),
     "location": "",
     "warning": "",
     "category": "Breath of the Wild",
@@ -76,7 +76,9 @@ def parse_physics(context, filepath):
     return {"FINISHED"}
 
 
-def generate_physics(context, filepath, vhacd, remove_hulls_after_export, binary=False):
+def generate_physics(
+    context, filepath, physics_type, vhacd, remove_hulls_after_export, binary=False
+):
     scene = bpy.context.scene
 
     if vhacd:
@@ -102,83 +104,147 @@ def generate_physics(context, filepath, vhacd, remove_hulls_after_export, binary
         filepath_yml = filepath
     script_file = os.path.realpath(__file__)
     directory = os.path.dirname(script_file)
-    default_file = directory + "/default.yml"
+    default_file = os.path.join(
+        directory, "weapon.yml" if physics_type == "WEAPON" else "default.yml"
+    )
     with open(default_file, "r") as f:
         content = f.read()
 
-    rigid_body_template = (
-        "                RigidBody_{0}: !list\n"
-        "                  objects:\n"
-        "                    948250248: !obj\n"
-        "                      rigid_body_name: !str64 {1}\n"
-        "                      mass: 10000.0\n"
-        "                      inertia: !vec3 [6666.67, 6666.67, 6666.67]\n"
-        "                      linear_damping: 0.0\n"
-        "                      angular_damping: 0.05\n"
-        "                      max_impulse: 10000.0\n"
-        "                      col_impulse_scale: 1.0\n"
-        "                      ignore_normal_for_impulse: false\n"
-        "                      volume: 8.0\n"
-        "                      toi: true\n"
-        "                      center_of_mass: !vec3 [0.0, 1.0, 0.0]\n"
-        "                      max_linear_velocity: 200.0\n"
-        "                      bounding_center: !vec3 [0.0, 1.0, 0.0]\n"
-        "                      bounding_extents: !vec3 [2.0, 2.0, 2.0]\n"
-        "                      max_angular_velocity_rad: 198.968\n"
-        "                      motion_type: !str32 Fixed\n"
-        "                      contact_point_info: !str32 Body\n"
-        "                      collision_info: !str32 Body\n"
-        "                      bone: !str64 \n"
-        "                      water_buoyancy_scale: 1.0\n"
-        "                      water_flow_effective_rate: 1.0\n"
-        "                      layer: !str32 EntityGroundObject\n"
-        "                      no_hit_ground: false\n"
-        "                      no_hit_water: false\n"
-        "                      groundhit: !str32 HitAll\n"
-        "                      use_ground_hit_type_mask: false\n"
-        "                      no_char_standing_on: false\n"
-        "                      navmesh: !str32 STATIC_WALKABLE_AND_CUTTING\n"
-        "                      navmesh_sub_material: !str32 \n"
-        "                      link_matrix: ''\n"
-        "                      magne_mass_scaling_factor: 1.0\n"
-        "                      always_character_mass_scaling: false\n"
-        "                      shape_num: {2}\n"
-        "{3}"
-        "                  lists: {{}}\n"
-    )
+    if physics_type == "WEAPON":
+        vertex_template = "                      vertex_{0}: !vec3 [{1}, {2}, {3}]"
 
-    shape_param_template = (
-        "                    ShapeParam_{}: !obj #{}\n"
-        "                      shape_type: !str32 polytope\n"
-        "                      vertex_num: {}\n"
-        "{}\n"
-        "                      material: !str32 Metal\n"
-        "                      sub_material: !str32 Metal_Heavy\n"
-        "                      wall_code: !str32 NoClimb\n"
-        "                      floor_code: !str32 None\n"
-    )
+        shape_template_metal = (
+            "                    ShapeParam_{0}: !obj\n"
+            "                      shape_type: !str32 polytope\n"
+            "                      vertex_num: {1}\n"
+            "{2}\n"
+            "                      material: !str32 Metal\n"
+            "                      sub_material: !str32 Metal\n"
+            "                      wall_code: !str32 None\n"
+            "                      floor_code: !str32 None\n"
+        )
 
-    vertex_template = "                      vertex_{}: !vec3 [{}, {}, {}]"
+        shape_template_u = (
+            "                    ShapeParam_{0}: !obj\n"
+            "                      shape_type: !str32 polytope\n"
+            "                      vertex_num: {1}\n"
+            "{2}\n"
+            "                      material: !str32 Undefined\n"
+            "                      sub_material: !str32 Undefined\n"
+            "                      wall_code: !str32 None\n"
+            "                      floor_code: !str32 None\n"
+        )
 
-    result = ""
+        scene = bpy.context.scene
+        hulls = [obj for obj in scene.objects if "_hull_" in obj.name]
 
-    rigid_bodies = ""
+        shapes_metal = ""
+        shapes_u = ""
+        for i, hull in enumerate(hulls):
+            mtx = mathutils.Matrix.Rotation(radians(-90.0), 4, "X") @ hull.matrix_world
+            vertices = [mtx @ v.co for v in hull.data.vertices]
+            shapes_metal += shape_template_metal.format(
+                i,
+                len(vertices),
+                "\n".join(
+                    [
+                        vertex_template.format(o, co.x, co.y, co.z)
+                        for o, co in enumerate(vertices)
+                    ]
+                ),
+            )
+            shapes_u += shape_template_u.format(
+                i,
+                len(vertices),
+                "\n".join(
+                    [
+                        vertex_template.format(o, co.x, co.y, co.z)
+                        for o, co in enumerate(vertices)
+                    ]
+                ),
+            )
 
-    non_hull_objects = [obj for obj in scene.objects if not ("_hull_" in obj.name)]
-    non_hull_index = 0
-    hulls = []
-    for obj in non_hull_objects:
-        if obj.type == "MESH":
+        output = content.format(len(hulls), shapes_metal, shapes_u)
+
+    elif physics_type in ("FIXED", "DYNAMIC"):
+        rigid_body_template = (
+            "                RigidBody_{0}: !list\n"
+            "                  objects:\n"
+            "                    948250248: !obj\n"
+            "                      rigid_body_name: !str64 {1}\n"
+            "                      mass: 10000.0\n"
+            "                      inertia: !vec3 [6666.67, 6666.67, 6666.67]\n"
+            "                      linear_damping: 0.0\n"
+            "                      angular_damping: 0.05\n"
+            "                      max_impulse: 10000.0\n"
+            "                      col_impulse_scale: 1.0\n"
+            "                      ignore_normal_for_impulse: false\n"
+            "                      volume: 8.0\n"
+            "                      toi: true\n"
+            "                      center_of_mass: !vec3 [0.0, 1.0, 0.0]\n"
+            "                      max_linear_velocity: 200.0\n"
+            "                      bounding_center: !vec3 [0.0, 1.0, 0.0]\n"
+            "                      bounding_extents: !vec3 [2.0, 2.0, 2.0]\n"
+            "                      max_angular_velocity_rad: 198.968\n"
+            "                      motion_type: !str32 {4}\n"
+            "                      contact_point_info: !str32 Body\n"
+            "                      collision_info: !str32 Body\n"
+            "                      bone: !str64 \n"
+            "                      water_buoyancy_scale: 1.0\n"
+            "                      water_flow_effective_rate: 1.0\n"
+            "                      layer: !str32 Entity{5}Object\n"
+            "                      no_hit_ground: false\n"
+            "                      no_hit_water: false\n"
+            "                      groundhit: !str32 HitAll\n"
+            "                      use_ground_hit_type_mask: false\n"
+            "                      no_char_standing_on: false\n"
+            "                      navmesh: !str32 {6}\n"
+            "                      navmesh_sub_material: !str32 \n"
+            "                      link_matrix: ''\n"
+            "                      magne_mass_scaling_factor: 1.0\n"
+            "                      always_character_mass_scaling: false\n"
+            "                      shape_num: {2}\n"
+            "{3}"
+            "                  lists: {{}}\n"
+        )
+
+        shape_param_template = (
+            "                    ShapeParam_{}: !obj #{}\n"
+            "                      shape_type: !str32 polytope\n"
+            "                      vertex_num: {}\n"
+            "{}\n"
+            "                      material: !str32 Metal\n"
+            "                      sub_material: !str32 Metal_Heavy\n"
+            "                      wall_code: !str32 NoClimb\n"
+            "                      floor_code: !str32 None\n"
+        )
+
+        vertex_template = "                      vertex_{}: !vec3 [{}, {}, {}]"
+
+        result = ""
+
+        rigid_bodies = ""
+
+        non_hull_objects = [
+            obj
+            for obj in scene.objects
+            if (not ("_hull_" in obj.name)) and (obj.type == "MESH")
+        ]
+        non_hull_index = 0
+        hulls = []
+        for obj in non_hull_objects:
             shape_hull_index = 0
             shapes = ""
             for shape_hull in set(scene.objects) - set(non_hull_objects):
                 if not (shape_hull.name.split("_hull_")[0] == obj.name):
                     continue
                 hulls.append(shape_hull)
+                # fmt: off
                 mtx = (
-                    mathutils.Matrix.Rotation(radians(-90.0), 4, "X")
+                    mathutils.Matrix.Rotation(radians(-90.0), 4, "X") \
                     @ shape_hull.matrix_world
                 )
+                # fmt: on
                 verts = [mtx @ v.co for v in shape_hull.data.vertices]
                 shapes += shape_param_template.format(
                     shape_hull_index,
@@ -193,10 +259,22 @@ def generate_physics(context, filepath, vhacd, remove_hulls_after_export, binary
                 )
                 shape_hull_index += 1
             rigid_bodies += rigid_body_template.format(
-                non_hull_index, obj.name, shape_hull_index, shapes
+                non_hull_index,
+                obj.name,
+                shape_hull_index,
+                shapes,
+                physics_type.capitalize(),
+                "Ground" if physics_type.capitalize() == "Fixed" else "",
+                "STATIC_WALKABLE_AND_CUTTING"
+                if physics_type.capitalize() == "Fixed"
+                else "DYNAMIC_SILHOUETTE_AND_OBSTACLE",
             )
             non_hull_index += 1
-    output = content.format(non_hull_index, rigid_bodies.rstrip("\n"))
+        output = content.format(non_hull_index, rigid_bodies.rstrip("\n"))
+    else:
+        ShowMessageBox("Something is wrong", "Huh?")
+        return {"CANCELLED"}
+
     with open(filepath_yml, "w") as output_file:
         output_file.write(output)
 
@@ -242,6 +320,21 @@ class ExportPhysics(Operator, ExportHelper):
 
     filter_glob: StringProperty(default="*.physics.yml", options={"HIDDEN"})
 
+    physics_type: EnumProperty(
+        name="Actor type",
+        description="Select actor type. Depending on what you select, a different file will be generated.",
+        items=(
+            (
+                "FIXED",
+                "Static/Structure",
+                "Static actor type (for buildings, static objects)",
+            ),
+            ("DYNAMIC", "Dynamic/Object", "Dynamic actor type (for moving actors)"),
+            ("WEAPON", "Weapon", "Weapon actor type (for swords)"),
+        ),
+        default="FIXED",
+    )
+
     vhacd: BoolProperty(
         name="Use V-HACD",
         description="Auto-generate collision using V-HACD (Disable if generated manually)",
@@ -258,6 +351,7 @@ class ExportPhysics(Operator, ExportHelper):
         return generate_physics(
             context,
             self.filepath,
+            physics_type=self.physics_type,
             vhacd=self.vhacd,
             remove_hulls_after_export=self.remove_hulls_after_export,
         )
@@ -266,6 +360,7 @@ class ExportPhysics(Operator, ExportHelper):
         layout = self.layout
         col = layout.column()
         col.label(text="Additional options:")
+        col.prop(self, "physics_type")
         col.prop(self, "vhacd")
         col.prop(self, "remove_hulls_after_export")
 
@@ -278,6 +373,21 @@ class ExportPhysicsBinary(Operator, ExportHelper):
     filename_ext = ".bphysics"
 
     filter_glob: StringProperty(default="*.bphysics", options={"HIDDEN"})
+
+    physics_type: EnumProperty(
+        name="Actor type",
+        description="Select actor type. Depending on what you select, a different file will be generated.",
+        items=(
+            (
+                "FIXED",
+                "Static/Structure",
+                "Static actor type (for buildings, static objects)",
+            ),
+            ("DYNAMIC", "Dynamic/Object", "Dynamic actor type (for moving actors)"),
+            ("WEAPON", "Weapon", "Weapon actor type (for swords)"),
+        ),
+        default="FIXED",
+    )
 
     vhacd: BoolProperty(
         name="Use V-HACD",
@@ -296,6 +406,7 @@ class ExportPhysicsBinary(Operator, ExportHelper):
             context,
             self.filepath,
             binary=True,
+            physics_type=self.physics_type,
             vhacd=self.vhacd,
             remove_hulls_after_export=self.remove_hulls_after_export,
         )
@@ -304,6 +415,7 @@ class ExportPhysicsBinary(Operator, ExportHelper):
         layout = self.layout
         col = layout.column()
         col.label(text="Additional options:")
+        col.prop(self, "physics_type")
         col.prop(self, "vhacd")
         col.prop(self, "remove_hulls_after_export")
 
